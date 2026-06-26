4b1d146f16d6a903f58c5a06b792efc7257529f9
EndermanbugZJFC <175898536+Ezjfc@users.noreply.github.com>
2026-06-27 04:01:49 +1000

    feat(lab): impl expand and collapse on fold button click

--- src/lab/Preview.tsx
+++ src/lab/Preview.tsx
@@ -2,7 +2,7 @@
  * @see Preview
  */
 import Gtk from "gi://Gtk?version=4.0";
-import { Accessor, createBinding, createComputed, createEffect, createState, Setter, With } from "gnim";
+import { Accessor, createBinding, createComputed, createState, Setter, With } from "gnim";
 import GObject from "gnim/gobject";
 import { BUTTON_PIXEL_SCALE } from "./app";
 import getExtMindustryIcon from "../libmindustrice/extMindustryIcon";
@@ -10,7 +10,6 @@ import PixelImage from "../libmindustrice/PixelImage";
 import FitEntry from "../libmindustrice/menu/FitEntry";
 import { $ } from "gnim-hooks";
 import GlyphIcon from "../libmindustrice/GlyphIcon";
-import Gly from "gi://Gly?version=2";
 
 /**
  * Parameters of a preview component.
@@ -47,6 +46,7 @@ export default function Preview({
   defaultHeight,
 }: Parameters) : GObject.Object {
   const [name, setName] = createState(defaultName)
+  const [collapsed, setCollapsed] = createState(false)
 
   return (
     <box orientation={Gtk.Orientation.VERTICAL} class="Preview">
@@ -54,8 +54,14 @@ export default function Preview({
         name={name}
         setName={setName}
         generateName={name}
+        setCollapsed={setCollapsed}
       />
-      <Resizer component={component} defaultWidth={defaultWidth} defaultHeight={defaultHeight} />
+      <Resizer
+        component={component}
+        defaultWidth={defaultWidth}
+        defaultHeight={defaultHeight}
+        visible={collapsed.as(c => !c)}
+      />
     </box>
   )
 }
@@ -63,16 +69,17 @@ export default function Preview({
 /**
  * Resizer initialises vertical and horizontal resizing panes that wraps the component.
  */
-function Resizer({ component, defaultWidth, defaultHeight }: {
+function Resizer({ component, defaultWidth, defaultHeight, visible }: {
   component: GObject.Object,
   defaultWidth?: number,
   defaultHeight?: number,
+  visible: Accessor<boolean>
 }) : GObject.Object {
   defaultWidth = defaultWidth ?? 500
   defaultHeight = defaultHeight ?? 40
 
   return (
-    <Gtk.Paned orientation={Gtk.Orientation.VERTICAL} position={defaultHeight} >
+    <Gtk.Paned orientation={Gtk.Orientation.VERTICAL} position={defaultHeight} visible={visible} >
       <Gtk.Paned orientation={Gtk.Orientation.HORIZONTAL} position={defaultWidth} >
           {component}
           <box hexpand={true} />
@@ -85,10 +92,11 @@ function Resizer({ component, defaultWidth, defaultHeight }: {
 /**
  * Toolbar initialises the toolbar in a preview component.
  */
-function Toolbar({ name, setName, generateName }: {
+function Toolbar({ name, setName, generateName, setCollapsed }: {
   name: Accessor<string>,
   setName: Setter<string>,
   generateName: Accessor<string>,
+  setCollapsed: Setter<boolean>,
 }) : GObject.Object {
   let getText: $<string>|undefined
   const placeholderVisible = createComputed(() => {
@@ -98,7 +106,7 @@ function Toolbar({ name, setName, generateName }: {
 
   return (
     <box class="Toolbar" valign={Gtk.Align.START} >
-    <FoldButton />
+    <FoldButton setCollapsed={setCollapsed} />
       <FitEntry
         $={(self) => getText = createBinding(self, "text")}
         appearence={{ noUnderline: true }}
@@ -123,11 +131,16 @@ function Toolbar({ name, setName, generateName }: {
 /**
  * FoldButton initialises a button that expands or collapses the preview on click.
  */
-function FoldButton() : GObject.Object {
+function FoldButton({ setCollapsed }: {
+  setCollapsed: Setter<boolean>,
+}) : GObject.Object {
   const [active, setActive] = createState(false)
 
   return (
-    <Gtk.ToggleButton onToggled={({ active }) => setActive(active)}>
+    <Gtk.ToggleButton onToggled={({ active }) => {
+      setActive(active)
+      setCollapsed(active)
+    }}>
       <With value={active}>
       {a => a ? (<GlyphIcon iconDownOpen />) : (<GlyphIcon iconUpOpen />)}
       </With>
